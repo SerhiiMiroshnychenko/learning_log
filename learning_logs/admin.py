ac29b14e4ca4054ab9f217112ffc9f4ce60d457c
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-11-15 17:08:32 +0200

    Database, topics and entry were added.

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Topic, Entry  # Імпортуємо модель, яку ми хочемо зареєструвати
+
+admin.site.register(Topic)  # даємо знати django що треба керувати моделлю через інтерфейс адміністратора
+admin.site.register(Entry)
