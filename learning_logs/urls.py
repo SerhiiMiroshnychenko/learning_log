478c980ce7dfac8e6b5a154598307085ac143cfd
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-11-20 16:34:09 +0200

    url mapping

---
+++ learning_logs/urls.py
@@ -0,0 +1,18 @@
+"""Defines URL patterns for learning_logs --- Визначає шаблони URL для learning_logs"""
+
+from django.urls import path  # Імпорт path - потрібна, щоб пов'язати URL з видами
+
+from . import views  # Імпорт модуля views. Крапка каже python шукати його в тій же самій теці
+
+app_name = 'learning_logs'  # Змінна app_name допомогає django відрізнити цей urls.py
+# від файлів з такою ж назвою в цьому проєкті
+urlpatterns = [  # Змінна urlpatterns - список індивідуальних сторінок,
+    # до яких можна звернутися із застосунку learning_logs:
+    # Головна сторінка
+    path('', views.index, name='index'),  # Функція path() повертає регулярний вираз URL і приймає
+    # три аргументи:
+    # перший(пустий string) - допомагає django правильно маршрутизувати поточний запит
+    # другий(views.index) - описує, яку функцію з views.py треба викликати.
+    # третій(name='index') - дає ім'я "index" цьому регулярному виразу URL
+]
+
