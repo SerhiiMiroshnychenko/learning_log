c4d88157ff2ddce8cc4c10193826cff73c65cc3d
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-11-20 17:40:29 +0200

    home page template OK

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here:
 def index(request):
     """Головна сторінка "Журналу спостережень"."""
-    return render(request, 'learning_log/index.html')
+    return render(request, 'learning_logs/index.html')
